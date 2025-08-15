9a8f2960d3acc060f001f05c1838e5dd9c4c7952
lkstudio.dante <lkstudio.dante@gmail.com>
2025-08-16 05:22:01 +0900

    no message

--- Engine/Unity/Example/Assets/04210221000101-Myongji_2nd_Programming_0001/Scripts/Editor/Global/Define/KEditor_Define+Global.cs
+++ Engine/Unity/Example/Assets/04210221000101-Myongji_2nd_Programming_0001/Scripts/Editor/Global/Define/KEditor_Define+Global.cs
@@ -16,7 +16,7 @@ public static partial class KEditor_Define
 	public const string G_PATTERN_NAME_SCENE = "t:Example t:Scene";
 
 	// 경로
-	public const string G_P_DIR_SEARCH_SCENE = "Assets/04210203000101-Myongji_2nd_Programming_0001/E01/Example/Scenes";
+	public const string G_P_DIR_SEARCH_SCENE = "Assets/04210221000101-Myongji_2nd_Programming_0001/E01/Example/Scenes";
 	#endregion // 컴파일 상수
 
 	#region 런타임 상수
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 04210203000101-Myongji_2nd_Programming_0001
+# 04210221000101-Myongji_2nd_Programming_0001
 명지대학교 2 학년 Programming - 2023 프로젝트
